c13543e6b353c8981e358ef91a29244e698dc704
johnst101 <tylercjohnson16@gmail.com>
2022-07-06 07:18:23 -0500

    pracitice loops and interview question

--- app.js
+++ app.js
@@ -69,3 +69,38 @@ let isStoreOpen = true;
 
 let stri = isStoreOpen && money >= cost ? 'give receipt' : 'do not give receipt';
 console.log(stri);
+
+// Loops
+
+// let count = 1;
+
+// while (count <= 100) {
+//   console.log(count);
+//   count = count + 1;
+// }
+
+// for (let i = 0; i < 3; i++) {
+//   console.log(i)
+// }
+
+/**Wrire a for-loop that loops through  1 to 20
+ * If the number is divisible by 3, print "Frontend"
+ * If the number is divisible by 5, print "Simplified"
+ * If the number is divisible by 3 and 5, print "Frontend Simplified"
+ * If the number is *not* divisible by eithe 3 or 5, print the number
+ */
+
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log(`${i} -> Frontend Simplified`)
+  }
+  else if (i % 3 === 0) {
+    console.log(`${i} -> Frontend`)
+  }
+  else if (i % 5 === 0) {
+    console.log(`${i} -> Simplified`)
+  }
+  else {
+    console.log(`${i} -> ${i}`)
+  }
+}
