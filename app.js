2010a58ec87d98d1a92e49f7663f5e74e31e7847
johnst101 <tylercjohnson16@gmail.com>
2022-07-05 20:17:42 -0500

    added practice equality test

--- app.js
+++ app.js
@@ -7,6 +7,14 @@
  */
 
 let celsius = 10;
-let fahrenheit = celsius * 1.8 + 32
+let fahrenheit = celsius * 1.8 + 32;
 
-console.log(fahrenheit)
+console.log(fahrenheit);
+
+// EQUALITY TEST
+
+let bool = '1' == 1;
+let bool2 = '1' === 1;
+
+console.log(bool);
+console.log(bool2);
