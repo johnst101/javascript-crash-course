8ce03836804296a7c27dab27c38bed971691dd38
johnst101 <tylercjohnson16@gmail.com>
2022-07-05 20:11:54 -0500

    practicing with converting temperatures exercise

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-let name = ''
+/**
+ * Converting Temperatures
+ * 
+ * Celsius-to-Fahrenheit formula:
+ * 
+ *  F = C x 1.8 + 32
+ */
+
+let celsius = 10;
+let fahrenheit = celsius * 1.8 + 32
+
+console.log(fahrenheit)
